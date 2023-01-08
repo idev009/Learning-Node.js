97b0f2759575b2f3fe2a83d209f14d64473ff26c
idev009 <idev0091@gmail.com>
2023-01-08 21:07:33 +0530

    Asynchronous JS

---
+++ Asynchronous JS/index.js
@@ -0,0 +1,13 @@
+// Problem
+
+// console.log("before");
+// const user = getUser(1);
+// console.log(user);
+// console.log("after");
+
+// function getUser(id) {
+//   setTimeout(() => {
+//     console.log("reading user from the database");
+//     return { id: id, user: "ishan" };
+//   }, 2000);
+// }
