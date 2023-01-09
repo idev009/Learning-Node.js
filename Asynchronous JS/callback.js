3731d2c484311ae93d11c34b41af5cde44fb2818
idev009 <idev0091@gmail.com>
2023-01-09 11:46:14 +0530

    Intro to Callbacks

---
+++ Asynchronous JS/callback.js
@@ -0,0 +1,27 @@
+console.log("before");
+getUser(1, (user) => {
+  console.log("user", user);
+  githubRepoUsername(user.name, (repo) => {
+    console.log("repo", repo);
+  });
+});
+
+console.log("After");
+
+function getUser(userId, callback) {
+  setTimeout(
+    () => {
+      console.log("reading user from database");
+      callback({ id: userId, name: "ishan" });
+    },
+
+    2000
+  );
+}
+
+function githubRepoUsername(user, callback) {
+  setTimeout(() => {
+    console.log("Calling Github API to fetch user repo");
+    callback(["repo1", "repo2", "repo3"]);
+  }, 3000);
+}
