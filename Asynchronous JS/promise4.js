60a18d99a56fbf1245677d51d3bffe8bec54868c
idev009 <idev0091@gmail.com>
2023-01-10 12:18:08 +0530

    Promises

---
+++ Asynchronous JS/promise4.js
@@ -0,0 +1,17 @@
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async Work 1");
+    resolve(1);
+  }, 2000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log("Async Work 2");
+    // reject(new Error("message"));
+  }, 2000);
+});
+
+Promise.all([p1, p2])
+  .then((result) => console.log("result", result))
+  .catch((err) => console.log(err));
