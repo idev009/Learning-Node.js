485b91480337630ae1d5a9bd1ee070fe06cc11f1
idev009 <idev0091@gmail.com>
2023-01-11 15:14:35 +0530

    Environmental Variables, Started Working with Mongo DB, Queries

---
+++ Asynchronous JS/MongoDB/index.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const uri = "mongodb://127.0.0.1:27017/playground";
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }) //connect method return promise
+  .then((result) => console.log("connected to mongo db"))
+  .catch((err) => console.log(err));
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+const Course = mongoose.model("Course", courseSchema);
+async function createCourse() {
+  const course = new Course({
+    name: "React course",
+    author: "Akshay Saini",
+    tags: ["react", "frontend"],
+    isPublished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function fetchRecord() {
+  const result = await Course.find().limit(10).sort({ name: 1 });
+  // .select({ name: 1, tags: 1 });
+  console.log(result);
+}
+
+// find record and filtering records
+fetchRecord();
+
+// save record
+// createCourse();
+
+// mongodb://localhost:27017
